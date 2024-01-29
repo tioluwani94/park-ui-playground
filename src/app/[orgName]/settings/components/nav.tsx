f0e704b6f7b4c4e82b5ac2e84c63587716865b40
Tioluwani Kolawole <kolawole8tiolu@gmail.com>
2024-01-29 11:20:40 +0100

    feat: add nav content component

--- src/app/[orgName]/settings/components/nav.tsx
+++ src/app/[orgName]/settings/components/nav.tsx
@@ -185,12 +185,88 @@ const NavMobile = (props: { children: ReactNode }) => {
   );
 };
 
+const NavContent = () => {
+  return (
+    <>
+      <NavHeader />
+      <NavSection>
+        <NavSectionHeader
+          label="Team"
+          icon={
+            <Icon css={{ w: "16px", h: "16px", color: "fg.muted" }}>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M2.25 12.75V12A2.25 2.25 0 0 1 4.5 9.75h15A2.25 2.25 0 0 1 21.75 12v.75m-8.69-6.44-2.12-2.12a1.5 1.5 0 0 0-1.061-.44H4.5A2.25 2.25 0 0 0 2.25 6v12a2.25 2.25 0 0 0 2.25 2.25h15A2.25 2.25 0 0 0 21.75 18V9a2.25 2.25 0 0 0-2.25-2.25h-5.379a1.5 1.5 0 0 1-1.06-.44Z"
+                />
+              </svg>
+            </Icon>
+          }
+        />
+        <NavList>
+          <NavListItem href={`/settings`}>Overview</NavListItem>
+          <NavListItem href={`/settings/team`}>Team</NavListItem>
+          <NavListItem href={`/settings/members`}>Members</NavListItem>
+          <NavListItem href={`/settings/plans`}>Plans</NavListItem>
+          <NavListItem href={`/settings/billing`}>Billing</NavListItem>
+        </NavList>
+      </NavSection>
+      <NavSection>
+        <NavSectionHeader
+          label="Account"
+          icon={
+            <Icon css={{ w: "16px", h: "16px", color: "fg.muted" }}>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-6 h-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+                />
+              </svg>
+            </Icon>
+          }
+        />
+        <NavList>
+          <NavListItem href={`/settings/profile`}>Profile</NavListItem>
+          <NavListItem href={`/settings/notifications`}>
+            Notifications
+          </NavListItem>
+        </NavList>
+      </NavSection>
+    </>
+  );
+};
+
 const List = NavList;
 const Item = NavListItem;
 const Header = NavHeader;
 const Mobile = NavMobile;
 const Section = NavSection;
+const Content = NavContent;
 const Container = NavContainer;
 const SectionHeader = NavSectionHeader;
 
-export { Container, Header, Item, List, Mobile, Section, SectionHeader };
+export {
+  Container,
+  Header,
+  Item,
+  List,
+  Mobile,
+  Section,
+  SectionHeader,
+  Content,
+};
